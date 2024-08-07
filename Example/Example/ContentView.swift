03aa19a483163f017a3445080d14154867959066
Ydna <0xEFEFEF@gmail.com>
2024-08-08 01:06:19 +0800

    refactor: preview all icon at once

--- Example/Example/ContentView.swift
+++ Example/Example/ContentView.swift
@@ -1,15 +1,46 @@
-import SwiftUI
 import FluentIcon
+import SwiftUI
 
 struct ContentView: View {
 	var body: some View {
-		VStack {
-			FluentIcon.globeAdd20Filled.image
-				.size(70)
-			Text("Hello, world!")
+		GeometryReader { proxy in
+			ScrollView {
+				let spacing = 8
+				let fullWidth = proxy.size.width
+				let columnCount = Int(Double(fullWidth) / 100)
+				let columnWidth = (fullWidth - CGFloat(columnCount + 3) * CGFloat(spacing)) / CGFloat(columnCount)
+
+				LazyVGrid(
+					columns: Array(
+						repeating: GridItem(
+							.fixed(columnWidth),
+							spacing: CGFloat(spacing)
+						),
+						count: columnCount
+					)
+				) {
+					ForEach(FluentIcon.allCases) { icon in
+						icon.image
+							.size(columnWidth - 20)
+							.frame(width: columnWidth, height: columnWidth)
+							.background(
+								RoundedRectangle(cornerRadius: 16, style: .continuous)
+									.foregroundStyle(Color(uiColor: .secondarySystemBackground))
+									.overlay(content: {
+										RoundedRectangle(cornerRadius: 16, style: .continuous)
+											.stroke(lineWidth: 1 / 3)
+											.foregroundStyle(Color(uiColor: .separator))
+									})
+							)
+					}
+				}
+			}
 		}
-		.padding()
 	}
+}
+
+extension FluentIcon: Identifiable {
+	public var id: String { rawValue }
 }
 
 #Preview {
