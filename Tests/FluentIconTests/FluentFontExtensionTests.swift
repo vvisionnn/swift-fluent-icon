507714bfcc69537c8e46315a89789a4419b1b23a
Ydna <0xEFEFEF@gmail.com>
2024-08-10 17:04:40 +0800

    test: run appkit test on on macOS

--- Tests/FluentIconTests/FluentFontExtensionTests.swift
+++ Tests/FluentIconTests/FluentFontExtensionTests.swift
@@ -1,3 +1,4 @@
+#if os(macOS)
 import AppKit
 @testable import FluentIcon
 import XCTest
@@ -8,16 +9,17 @@ final class FluentFontExtensionTests: XCTestCase {
 		let fontNames = Fluent.Font.fontNames(forFamilyName: fontFamily)
 		XCTAssertFalse(fontNames.isEmpty, "Font names should not be empty for existing font family \(fontFamily)")
 	}
-
+	
 	func testFontNamesForNonExistingFamily() {
 		let fontFamily = "NonExistingFontFamily"
 		let fontNames = Fluent.Font.fontNames(forFamilyName: fontFamily)
 		XCTAssertTrue(fontNames.isEmpty, "Font names should be empty for non-existing font family \(fontFamily)")
 	}
-
+	
 	func testFontNamesForEmptyFamilyName() {
 		let fontFamily = ""
 		let fontNames = Fluent.Font.fontNames(forFamilyName: fontFamily)
 		XCTAssertTrue(fontNames.isEmpty, "Font names should be empty for empty font family name")
 	}
 }
+#endif
